7ffee9fc5c6c6906b5830393680ae24514bc7926
Vivek Kumar Patel <vivekece1116@gmail.com>
2024-02-24 10:35:27 +0530

    router part is done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Header from './components/Header';
+import Footer from './components/Footer.jsx';
+import Home from "./pages/Home.jsx"
+import Cart from "./pages/Cart.jsx"
+import Order from "./pages/Order.jsx"
+import Signup from "./pages/Sign.jsx"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Header/>
+      <Routes>
+        <Route path="/home" element={<Home/>}/>
+        <Route path="/cart" element={<Cart/>}/>
+        <Route path="/order" element={<Order/>}/>
+        <Route path="/signup" element={<Signup/>}/>
+      </Routes>
+      <Footer/>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/Sign.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Sign = () => {
+    return (
+        <div>
+            SignUp..
+        </div>
+    )
+}
+
+export default Sign
