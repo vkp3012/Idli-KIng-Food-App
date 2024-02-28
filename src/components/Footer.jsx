c6679293143bea213297d4b306354db1bdc06222
Vivek Kumar Patel <vivekece1116@gmail.com>
2024-02-28 07:19:01 +0530

    loging page is done

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,8 +2,12 @@ import React from 'react'
 
 const Footer = () => {
     return (
-        <div>
-            Footer....
+        <div className='container-fluid bg-dark text-light p-2 footer'>
+            <div className='container camera-icon'>
+                <div className='row d-flex justify-content-center align-items-center'>
+                    Idli King | 2023-2024 | © Copy Right
+                </div>
+            </div>
         </div>
     )
 }
